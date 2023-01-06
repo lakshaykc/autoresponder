0c7e1e723da3df422bf545bd3b8c38f770d4a32f
Kyle Corbitt <kyle@corbt.com>
2023-01-06 22:40:44 +0000

    make autoresponder public

---
+++ finetune-t5-large.py
@@ -0,0 +1,91 @@
+from transformers import (
+    Seq2SeqTrainer,
+    Seq2SeqTrainingArguments,
+    AutoTokenizer,
+    AutoModelForSeq2SeqLM,
+)
+import evaluate
+import nltk
+import numpy as np
+from nltk.tokenize import sent_tokenize
+from datasets import load_from_disk
+
+nltk.download("punkt")
+
+# Load the dataset from the ./data/chat_history folder
+ds = load_from_disk("data/chat_history")
+
+model_id = "google/flan-t5-large"
+model_dir = "models/flan-t5-large-10ep"
+
+# Metric
+metric = evaluate.load("rouge")
+
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+model = AutoModelForSeq2SeqLM.from_pretrained(model_id)
+
+# helper functions to postprocess text. Adapted from https://www.philschmid.de/fine-tune-flan-t5
+def postprocess_text(preds, labels):
+    preds = [pred.strip() for pred in preds]
+    labels = [label.strip() for label in labels]
+
+    # rougeLSum expects newline after each sentence
+    preds = ["\n".join(sent_tokenize(pred)) for pred in preds]
+    labels = ["\n".join(sent_tokenize(label)) for label in labels]
+
+    return preds, labels
+
+
+def compute_metrics(eval_preds):
+    preds, labels = eval_preds
+    if isinstance(preds, tuple):
+        preds = preds[0]
+    decoded_preds = tokenizer.batch_decode(preds, skip_special_tokens=True)
+    # Replace -100 in the labels as we can't decode them.
+    labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+    decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+
+    # Some simple post-processing
+    decoded_preds, decoded_labels = postprocess_text(decoded_preds, decoded_labels)
+
+    result = metric.compute(
+        predictions=decoded_preds, references=decoded_labels, use_stemmer=True
+    )
+    result = {k: round(v * 100, 4) for k, v in result.items()}
+    prediction_lens = [
+        np.count_nonzero(pred != tokenizer.pad_token_id) for pred in preds
+    ]
+    result["gen_len"] = np.mean(prediction_lens)
+    return result
+
+
+training_args = Seq2SeqTrainingArguments(
+    output_dir=model_dir,
+    per_device_train_batch_size=4,
+    per_device_eval_batch_size=4,
+    predict_with_generate=True,
+    fp16=False,
+    learning_rate=5e-5,
+    num_train_epochs=10,
+    # logging & evaluation strategies
+    logging_dir=f"{model_dir}/logs",
+    logging_strategy="steps",
+    logging_steps=2000,
+    evaluation_strategy="steps",
+    save_strategy="steps",
+    save_steps=2000,
+    save_total_limit=5,
+    load_best_model_at_end=True,
+    metric_for_best_model="rouge1",
+    report_to="tensorboard",
+)
+
+trainer = Seq2SeqTrainer(
+    model=model,
+    args=training_args,
+    train_dataset=ds["train"],
+    eval_dataset=ds["test"],
+    compute_metrics=compute_metrics,
+)
+
+trainer.train()
